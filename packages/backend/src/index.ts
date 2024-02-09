73907ddc943beba0f1133588103e205a8941881d
Onur Ravli <onur@onurravli.com>
2024-02-09 08:45:09 +0300

    feat(backend): use usersRouter in entrypoint

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,12 @@
 import express, { type Application } from 'express';
 import { env } from './utils';
 import { guvercin } from './services';
+import { usersRouter } from './routers';
 
 const app: Application = express();
 const port: number = parseInt(env.PORT as string) || 3001;
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   guvercin.info(`Server listening on port ${port}.`);
